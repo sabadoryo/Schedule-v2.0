ead051823274d57d16fa7c55af2b520381af452f
Yernar Abilbay <eabilbay@gmail.com>
2020-04-13 15:43:34 +0600

    some models + template added

--- schedule3/models.py
+++ schedule3/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Subjects(models.Model):
+    title = models.CharField(max_length=200)
+
+
+class Groups(models.Model):
+    title = models.CharField(max_length=6)
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
