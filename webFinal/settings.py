d0e129ac48a65566e0b6db7ac65cd420f6eb4c3d
Yernar Abilbay <eabilbay@gmail.com>
2020-05-26 19:01:49 +0600

    Init

--- webFinal/settings.py
+++ webFinal/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -22,9 +22,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4=rpw3s9xg6y@$+zi^e(r#$n7211yv8=-9ls(=+zx6u&g3+_8s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', None)
+if DEBUG is None:
+    DEBUG = False
+else:
+    if 'true' == DEBUG.lower():
+        DEBUG = True
+    else:
+        DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
